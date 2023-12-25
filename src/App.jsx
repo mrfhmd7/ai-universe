3966b7abff1076a086039a37494ed559a1410f33
mrfhmd7 <mrf.hmd7@gmail.com>
2023-12-26 02:21:34 +0600

    Card and SingleData component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import Button from "./components/Button/Button";
+import Card from "./components/Card/Card";
 
 const App = () => {
   return (
     <>
       <Header/>
       <Button>Sort by Date</Button>
+      <Card></Card>
       {/* <Button>See More</Button> */}
     </>
   );
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import SingleData from '../SingleData/SingleData';
+
+const Card = () => {
+     const [values, setValues] = useState([]);
+
+     useEffect(() => {
+          const loadData = async () => {
+               const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
+               const data = await res.json();
+               // console.log(data.data.tools);
+               setValues(data.data.tools);
+          }
+          loadData();
+     }, []);
+     return (
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-8 lg:px-10'>
+               {
+                    values.map(value => <SingleData
+                         value={value}
+                         key={value.id}
+                    ></SingleData>)
+               }
+          </div>
+     );
+};
+
+export default Card;
