0c4da3aeeaf45d9ce2a177c830cf169d9cd9e5d0
mrfhmd7 <mrf.hmd7@gmail.com>
2023-12-26 13:10:10 +0600

    card sytle added

--- src/components/SingleData/SingleData.jsx
+++ src/components/SingleData/SingleData.jsx
@@ -4,8 +4,8 @@ const SingleData = (props) => {
 //   console.log(props.value.image);
   const { image, features, name, published_in } = props.value;
   return (
-    <div>
-          <div className="card card-compact w-full bg-base-100 shadow-2xl">
+    <>
+          <div className="card card-compact w-full h-auto bg-base-100 shadow-2xl">
                <figure>
                     <img className="w-full h-64"
                          src={image}
@@ -14,19 +14,20 @@ const SingleData = (props) => {
                </figure>
                <div className="card-body">
                     <h2 className="card-title font-semibold">Features</h2>
-                      {
+                    {
                            features.map((feature,index) =>
                           <p>
                               {index+1}. {feature}  
                          </p>)
                     }  
-                    <p></p>
-                    <div className="card-actions justify-end">
+                    <hr className="border-2 border-stone-400 my-3" />
+                    <h2 className="font-bold text-2xl">{name}</h2>
+                    {/* <div className="card-actions justify-end">
                          <button className="btn btn-primary">Buy Now</button>
-                    </div>
+                    </div> */}
                </div>
           </div>
-    </div>
+    </>
   );
 };
 
